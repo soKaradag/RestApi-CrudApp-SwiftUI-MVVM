af1794b0804e646ba6392900e0b56c01f55f1ae9
serdar <sok.karadag@gmail.com>
2023-08-17 00:28:33 +0300

    Initial Commit

---
+++ RestApi-CrudApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RestApi-CrudApp
+//
+//  Created by Serdar Onur KARADAĞ on 17.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestApi-CrudApp/RestApi_CrudAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestApi_CrudAppApp.swift
+//  RestApi-CrudApp
+//
+//  Created by Serdar Onur KARADAĞ on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestApi_CrudAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
